ae1bec4c2c28f2e040d5fee2fbf80a83d897d3c9
Thomas Hilaire <t.hilaire@optimiz.me>
2020-11-10 01:10:17 +0100

    Make d3 binding allowing mouse events

--- src/graph/CalendarGraph.js
+++ src/graph/CalendarGraph.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import * as d3 from 'd3'
 import {legend} from "./monthlyLegend";
 
@@ -31,65 +31,67 @@ const colorResolver = years => {
 export function CalendarGraph(props) {
     const {color, legendBuilder} = colorResolver(props.years);
 
-    const svg = d3.create("svg")
-        .attr("viewBox", [0, 0, width, (height + (props.selectedRegions.length * cellSize)) * props.years.length])
-        .attr("font-family", "sans-serif")
-        .attr("font-size", 10);
+    useEffect(() => {
+        d3.select("#calendar-legend").append(() => legendBuilder());
 
-    const yearSvg = svg.selectAll("g")
-        .data(props.years)
-        .join("g")
-        .attr("transform", (d, i) => `translate(345,${(40 + (props.selectedRegions.length * cellSize)) * i + cellSize * 1.5})`);
+        const svg = d3.select("#calendar-graph")
+            .attr("viewBox", [0, 0, width, (40 + (props.selectedRegions.length * cellSize)) * props.years.length])
+            .attr("font-family", "sans-serif")
+            .attr("font-size", 10);
 
-    yearSvg.append("text")
-        .attr("x", -5)
-        .attr("y", -5)
-        .attr("font-weight", "bold")
-        .attr("text-anchor", "end")
-        .text(year => year.name);
+        const yearSvg = svg.selectAll("g")
+            .data(props.years)
+            .join("g")
+            .attr("transform", (d, i) => `translate(345,${(40 + (props.selectedRegions.length * cellSize)) * i + cellSize * 1.5})`);
 
-    yearSvg.append("g")
-        .attr("text-anchor", "end")
-        .selectAll("text")
-        .data(props.selectedRegions)
-        .join("text")
-        .attr("x", -5)
-        .attr("y", (_, index) => (index + 0.5) * cellSize)
-        .attr("dy", "0.31em")
-        .text(key => key);
+        yearSvg.append("text")
+            .attr("x", -5)
+            .attr("y", -5)
+            .attr("font-weight", "bold")
+            .attr("text-anchor", "end")
+            .text(year => year.name);
+
+        yearSvg.append("g")
+            .attr("text-anchor", "end")
+            .selectAll("text")
+            .data(props.selectedRegions)
+            .join("text")
+            .attr("x", -5)
+            .attr("y", (_, index) => (index + 0.5) * cellSize)
+            .attr("dy", "0.31em")
+            .text(key => key);
 
 
-    yearSvg.append("g")
-        .selectAll("g")
-        .data(['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'])
-        .join("g")
-        .append("text")
-        .attr("x", (_, index) => index * cellSize + 20)
-        .attr("y", -5)
-        .text(month => month);
+        yearSvg.append("g")
+            .selectAll("g")
+            .data(['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'])
+            .join("g")
+            .append("text")
+            .attr("x", (_, index) => index * cellSize + 20)
+            .attr("y", -5)
+            .text(month => month);
 
-    yearSvg.append("g")
-        .selectAll("rect")
-        .data(year => year.values)
-        .join("rect")
-        .attr("width", cellSize - 1)
-        .attr("height", cellSize - 1)
-        .attr("x", cell => cell.month * cellSize + 0.5)
-        .attr("y", cell => {
-            const regionIndex = props.selectedRegions.findIndex(selected => selected === cell.regionOrDistrict);
-            return regionIndex * cellSize + 0.5;
-        })
-        .attr("fill", cell => color(cell.coverageRateDiff))
-        .append("title")
-        .text(cell => `${props.indicator} for ${cell.regionOrDistrict}: 
+        yearSvg.append("g")
+            .selectAll("rect")
+            .data(year => year.values)
+            .join("rect")
+            .attr("width", cellSize - 1)
+            .attr("height", cellSize - 1)
+            .attr("x", cell => cell.month * cellSize + 0.5)
+            .attr("y", cell => {
+                const regionIndex = props.selectedRegions.findIndex(selected => selected === cell.regionOrDistrict);
+                return regionIndex * cellSize + 0.5;
+            })
+            .attr("fill", cell => color(cell.coverageRateDiff))
+            .append("title")
+            .text(cell => `${props.indicator} for ${cell.regionOrDistrict}: 
 - Coverage rate diff: ${cell.coverageRateDiff}
 - Target population: ${cell.targetPop}
 - Value: ${cell.value}`);
-
-    const node = svg.node();
+    }, []);
 
     return <>
-        <div style={{width: legendWidth, margin: '25px auto auto'}} dangerouslySetInnerHTML={{__html: legendBuilder().outerHTML}}></div>
-        <div dangerouslySetInnerHTML={{__html: node.outerHTML}}></div>
+        <div id="calendar-legend" style={{width: legendWidth, margin: '25px auto auto'}}></div>
+        <svg id="calendar-graph"></svg>
     </>;
 }
